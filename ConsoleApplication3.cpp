ae0fc5434709d9238a4865d8eed10893f9fed689
JunSun <63651740+Veronika2013@users.noreply.github.com>
2021-01-14 19:21:05 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <istream>
+#include <ostream>
+#include <string>
+#include <boost/thread.hpp>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+using namespace boost::asio;
+io_service service;
+void dataSci()
+{
+	ip::tcp::resolver resolver(service);
+	ip::tcp::resolver::query query("www.vk.com", "80");
+	ip::tcp::resolver::iterator iter = resolver.resolve(query);
+	ip::tcp::endpoint ep = *iter; 
+	std::cout << ep.address().to_string() << std::endl;
+	std::cout << ep.data() << std::endl;
+	std::cout << ep.protocol().v4 << std::endl;
+	std::cout << ep.size() << std::endl;
+}
+void asio_proto()
+{
+
+}
+int main(int argc, char* argv[]) {
+	dataSci();
+
+}
